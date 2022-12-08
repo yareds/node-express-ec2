92983fbf5d3a7556efe1ed6e78b0405ad8cc85e3
yareds <37346809+yareds@users.noreply.github.com>
2022-12-07 23:33:17 -0500

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-import express from "express";
+//import express from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+const fs = require("fs");
+
+// configure our express instance with some body-parser settings
+// including handling JSON data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// this is where we'll handle our various routes from
+const routes = require("./routes/routes.js")(app, fs);
 
 app.listen(3001, () => console.log("Api is running on port 3001"));
 
